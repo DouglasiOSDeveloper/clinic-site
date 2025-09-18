a66d14464ee6e935aedc40f745712de275b46131
DouglasiOSDeveloper <douguerpw2@hotmail.com>
2025-09-18 16:40:15 -0300

    DISABLE PPR

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  experimental: {
+    ppr: false, // <- desativa Partial Prerendering e evita o erro do clientReferenceManifest
+  },
 };
 
 export default nextConfig;
