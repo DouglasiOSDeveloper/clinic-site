9486ad9166066392f80034f93d8a4f8f8539e4b9
DouglasiOSDeveloper <douguerpw2@hotmail.com>
2025-09-18 15:42:21 -0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import "../styles/globals.css";
+import "@/styles/globals.css";
 import { Playfair_Display, Inter } from "next/font/google";
 
 const heading = Playfair_Display({ subsets: ["latin"], variable: "--font-heading" });
